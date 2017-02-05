4b3ef096fcbcabd3fabdbe9f4014c9e1c86643d1
yehengjia <yehengjia@gmail.com>
2017-02-05 18:47:26 +0800

    no message

--- app/src/main/java/com/example/yenhenchia/projectpractice/SimpleIntent/SimpleIntentActivity.java
+++ app/src/main/java/com/example/yenhenchia/projectpractice/SimpleIntent/SimpleIntentActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.yenhenchia.projectpractice.MainActivity;
 import com.example.yenhenchia.projectpractice.R;
@@ -16,7 +17,8 @@ import java.util.ArrayList;
 
 public class SimpleIntentActivity extends AppCompatActivity {
 
-    private Button btnWeb, btnMp3, btnImage, btnSendData;
+    private Button btnWeb, btnMp3, btnImage, btnSendData, btnReceiveData;
+    private TextView txtReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,11 @@ public class SimpleIntentActivity extends AppCompatActivity {
 
         this.btnSendData = (Button) findViewById(R.id.btnSendData);
         this.btnSendData.setOnClickListener(onSendClickListener);
+
+        this.btnReceiveData = (Button) findViewById(R.id.btnReceiveData);
+        this.btnReceiveData.setOnClickListener(onReceiveClickListener);
+
+        this.txtReceive = (TextView) findViewById(R.id.txtReceive);
     }
 
     private Button.OnClickListener onWebClickListener = new Button.OnClickListener() {
@@ -112,4 +119,61 @@ public class SimpleIntentActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
+    private Button.OnClickListener onReceiveClickListener = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent();
+            intent.setClass(SimpleIntentActivity.this, SimpleIntentReceivedActivity.class);
+            startActivityForResult(intent, SimpleIntentReceivedActivity.RECEIVE_STATE_START);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != SimpleIntentReceivedActivity.RECEIVE_STATE_START) {
+
+            return;
+        }
+
+        switch (resultCode) {
+
+            case SimpleIntentReceivedActivity.RECEIVE_STATE_RECEIVE1: {
+
+                String s = "RECEIVE_STATE_RECEIVE1 : ";
+
+                if (data != null) {
+
+                    Bundle bundle = data.getExtras();
+
+                    if (bundle != null) {
+
+                        String str = bundle.getString("String");
+
+                        s += str;
+                    }
+                }
+
+                txtReceive.setText(s);
+            }
+                break;
+            case SimpleIntentReceivedActivity.RECEIVE_STATE_RECEIVE2: {
+
+                String s = "RECEIVE_STATE_RECEIVE2 : ";
+
+                if (data != null) {
+
+                    String str = data.getStringExtra("String");
+                    s += str;
+                }
+
+                txtReceive.setText(s);
+            }
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/yenhenchia/projectpractice/SimpleIntent/SimpleIntentReceivedActivity.java
+++ app/src/main/java/com/example/yenhenchia/projectpractice/SimpleIntent/SimpleIntentReceivedActivity.java
@@ -3,6 +3,8 @@ package com.example.yenhenchia.projectpractice.SimpleIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.yenhenchia.projectpractice.R;
@@ -13,10 +15,22 @@ public class SimpleIntentReceivedActivity extends AppCompatActivity {
 
     private TextView txtIntentResult;
 
+    public final static int RECEIVE_STATE_START  = 0;
+    public final static int RECEIVE_STATE_RECEIVE1  = 1;
+    public final static int RECEIVE_STATE_RECEIVE2  = 2;
+
+    private Button btnReceive1, btnReceive2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_intent_received);
+
+        this.btnReceive1 = (Button) findViewById(R.id.bntReceiveResult1);
+        this.btnReceive1.setOnClickListener(onReceive1ClickListener);
+
+        this.btnReceive2 = (Button) findViewById(R.id.bntReceiveResult2);
+        this.btnReceive2.setOnClickListener(onReceive2ClickListener);
 
         this.txtIntentResult = (TextView) findViewById(R.id.txtIntentResult);
         showResult();
@@ -30,7 +44,7 @@ public class SimpleIntentReceivedActivity extends AppCompatActivity {
 
         Bundle bundle = intent.getExtras();
 
-        if (!bundle.isEmpty()) {
+        if (bundle != null && !bundle.isEmpty()) {
 
             int value = bundle.getInt("Int");
             String s = bundle.getString("String");
@@ -55,32 +69,74 @@ public class SimpleIntentReceivedActivity extends AppCompatActivity {
 
             this.txtIntentResult.setText(str);
         }
-        else if (scheme.equals("http")) {
+        else if (scheme != null && scheme.equals("http")) {
 
-            String str = "You receive a request of intent to show web view, data is : " + intent.getData().toString();
+            if (intent.getData() != null) {
 
-            this.txtIntentResult.setText(str);
+                String str = "You receive a request of intent to show web view, data is : " + intent.getData().toString();
+
+                this.txtIntentResult.setText(str);
+            }
         }
-        else if(scheme.equals("tel")) {
+        else if(scheme != null && scheme.equals("tel")) {
 
-            String str = "You receive a request of intent to call phone, data is : " + intent.getData().toString();
+            if (intent.getData() != null) {
 
-            this.txtIntentResult.setText(str);
+                String str = "You receive a request of intent to call phone, data is : " + intent.getData().toString();
+
+                this.txtIntentResult.setText(str);
+            }
         }
-        else if(scheme.equals("file")) {
+        else if(scheme != null && scheme.equals("file")) {
 
             if (act.equals("android.intent.action.VIEW")) {
 
-                String str = "You receive a request of intent to show view, data is : " + intent.getData().toString();
+                if (intent.getData() != null) {
 
-                this.txtIntentResult.setText(str);
+                    String str = "You receive a request of intent to show view, data is : " + intent.getData().toString();
+
+                    this.txtIntentResult.setText(str);
+                }
             }
             else if(act.equals("android.intent.action.EDIT")) {
 
-                String str = "You receive a request of intent to edit view, data is : " + intent.getData().toString();
+                if (intent.getData() != null) {
 
-                this.txtIntentResult.setText(str);
+                    String str = "You receive a request of intent to edit view, data is : " + intent.getData().toString();
+
+                    this.txtIntentResult.setText(str);
+                }
             }
         }
     }
+
+    private Button.OnClickListener onReceive1ClickListener = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("String", "result1");
+
+            intent.putExtras(bundle);
+
+            setResult(RECEIVE_STATE_RECEIVE1, intent);
+            finish();
+        }
+    };
+
+    private Button.OnClickListener onReceive2ClickListener = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent();
+            intent.putExtra("String", "result2");
+
+            setResult(RECEIVE_STATE_RECEIVE2, intent);
+            finish();
+        }
+    };
 }
