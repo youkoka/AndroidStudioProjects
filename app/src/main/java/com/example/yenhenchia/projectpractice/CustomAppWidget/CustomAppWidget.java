9c358b5e4ef34e4634d047aad2a20caed4bc457c
yehengjia <yehengjia@gmail.com>
2017-02-12 15:27:53 +0800

    Custom App Widget

---
+++ app/src/main/java/com/example/yenhenchia/projectpractice/CustomAppWidget/CustomAppWidget.java
@@ -0,0 +1,15 @@
+package com.example.yenhenchia.projectpractice.CustomAppWidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yenhenchia.projectpractice.R;
+
+public class CustomAppWidget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_app_widget);
+    }
+}
