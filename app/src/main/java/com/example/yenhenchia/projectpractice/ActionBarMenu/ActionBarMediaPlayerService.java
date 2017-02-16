42016bff2bc36acde55905ee974c438523300f75
YenHenChia <yehengjia@gmail.com>
2017-02-16 14:34:46 +0800

    action bar option menu not implement

--- app/src/main/java/com/example/yenhenchia/projectpractice/ActionBarMenu/ActionBarMediaPlayerService.java
+++ app/src/main/java/com/example/yenhenchia/projectpractice/ActionBarMenu/ActionBarMediaPlayerService.java
@@ -2,15 +2,69 @@ package com.example.yenhenchia.projectpractice.ActionBarMenu;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 
+import com.example.yenhenchia.projectpractice.SimpleService.MyService;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.URI;
+
 public class ActionBarMediaPlayerService extends Service {
+
+    private MediaPlayer mediaPlayer;
+
     public ActionBarMediaPlayerService() {
     }
+
+    public class LocalBinder extends Binder {
+
+        ActionBarMediaPlayerService getService() {
+
+            return ActionBarMediaPlayerService.this;
+        }
+    }
+
+    private LocalBinder localBinder = new LocalBinder();
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        return localBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        String mp3File = Environment.getExternalStorageDirectory().getPath() +
+                File.separator + "Music" +
+                File.separator +"9.BONJOUR!.mp3";
+        File file = new File(mp3File);
+        if (file.exists()) {
+
+            Uri uri = Uri.fromFile(file);
+
+            Intent it = new Intent(Intent.ACTION_VIEW);
+
+            it.setDataAndType(Uri.fromFile(file), "audio/*");
+
+            startActivity(it);
+//            mediaPlayer = MediaPlayer.create(this, uri);
+//            mediaPlayer.start();
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mediaPlayer.stop();
     }
 }
--- app/src/main/java/com/example/yenhenchia/projectpractice/ActionBarMenu/ActionBarMenuActivity.java
+++ app/src/main/java/com/example/yenhenchia/projectpractice/ActionBarMenu/ActionBarMenuActivity.java
@@ -1,5 +1,8 @@
 package com.example.yenhenchia.projectpractice.ActionBarMenu;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,13 +11,16 @@ import android.view.SubMenu;
 
 import com.example.yenhenchia.projectpractice.R;
 
+import java.io.File;
+
 public class ActionBarMenuActivity extends AppCompatActivity {
 
     private static final int MENU_MUSIC = Menu.FIRST,
                              MENU_PALY_MUSIC = Menu.FIRST + 1,
                              MENU_STOP_PLAYING_MENU = Menu.FIRST + 2,
                              MENU_ABOUT = Menu.FIRST + 3,
-                             MENU_EXIT = Menu.FIRST + 4;
+                             MENU_TEXTLENGTH = Menu.FIRST + 4,
+                             MENU_EXIT = Menu.FIRST + 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +30,53 @@ public class ActionBarMenuActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        //        menu.add(0, MENU_ABOUT, 1, "關於這個程式...").setIcon(android.R.drawable.ic_dialog_info);
         SubMenu subMenu = menu.addSubMenu(0, MENU_MUSIC, 0, "背景音樂").setIcon(android.R.drawable.ic_media_ff);
-//        SubMenu subMenu = menu.addSubMenu(0, MENU_MUSIC, 0, "背景音樂");
+        subMenu.add(0, MENU_PALY_MUSIC, 0, "播放背景音樂");
+        subMenu.add(0, MENU_STOP_PLAYING_MENU, 1, "停止播放背景音樂");
+
+        menu.add(0, MENU_TEXTLENGTH, 1, "測試字串長度測試字串長度測試字串長度測試字串長度測試字串長度測試字串長度測試字串長度測試字串長度").setIcon(android.R.drawable.ic_dialog_info);
+        menu.add(0, MENU_ABOUT, 2, "關於這個程式").setIcon(android.R.drawable.ic_dialog_info);
+        menu.add(0, MENU_EXIT, 3, "結束").setIcon(android.R.drawable.ic_menu_close_clear_cancel);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+//        item.getGroupId()
+
+        switch (item.getItemId()) {
+
+            case MENU_PALY_MUSIC: {
+
+//                Intent intent = new Intent(ActionBarMenuActivity.this, ActionBarMediaPlayerService.class);
+//                startService(intent);
+
+                String mp3File = Environment.getExternalStorageDirectory().getPath() +
+                        File.separator + "Music" +
+                        File.separator +"9.BONJOUR!.mp3";
+                File file = new File(mp3File);
+                if (file.exists()) {
+
+                    Intent it = new Intent(Intent.ACTION_VIEW);
+                    it.setDataAndType(Uri.fromFile(file), "audio/*");
+                    startActivity(it);
+                }
+            }
+                break;
+            case MENU_STOP_PLAYING_MENU: {
+
+                Intent intent = new Intent(ActionBarMenuActivity.this, ActionBarMediaPlayerService.class);
+                stopService(intent);
+            }
+                break;
+            case MENU_EXIT: {
+
+                finish();
+            }
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
